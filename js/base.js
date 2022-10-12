26a66e5ccc9e3b7c10f509b07ff69a5420d252e4
KHATRIVINAY1 <khatrivinay1@gmail.com>
2022-10-12 20:55:03 +0530

    complete index

---
+++ js/base.js
@@ -0,0 +1,109 @@
+
+// fixed nav bar
+window.addEventListener("scroll", ()=>{
+    let header = document.querySelector("header");
+    header.classList.toggle("fixed-header", window.scrollY>0)
+})
+
+
+
+function toggleNavbar(){
+    let mobNavbar = document.querySelector(".nav-list");
+    mobNavbar.classList.toggle("toggel-nav")
+    this.style.zIndex="2"
+    if(mobNavbar.classList.contains("toggel-nav"))
+        this.innerHTML =`<i class="bi bi-x-circle"></i>`
+    else
+        {
+            this.innerHTML = `<i class="bi bi-list-nested"></i>`
+    }
+ }
+
+
+let navBarToggler = document.querySelector(".nav-toggle-btn")
+
+navBarToggler.addEventListener('click', toggleNavbar)
+
+// review section
+const reviews = [
+    {
+      id: 1,
+      name: "susan smith",
+      job: "web developer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+      text:
+        "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+      id: 2,
+      name: "anna johnson",
+      job: "web designer",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      text:
+        "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+      id: 3,
+      name: "peter jones",
+      job: "intern",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+      text:
+        "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+      id: 4,
+      name: "bill anderson",
+      job: "the boss",
+      img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      text:
+        "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+  ];
+
+let currentitem = 1
+
+const reviewPerson=(currentitem)=>{
+    user = reviews[currentitem-1];
+    let userImg = document.querySelector('.user-img')
+    let username = document.querySelector('.username')
+    let quote = document.querySelector('.user-quote')
+    let location = document.querySelector('.user-location')
+    username.innerHTML = user.name;
+    quote.innerHTML = user.text;
+    location.innerHTML = user.job
+    userImg.src = user.img
+}
+
+window.addEventListener("DOMContentLoaded", ()=>{
+    reviewPerson(currentitem);
+})
+
+
+const nextBtn = document.querySelector('#next');
+const prevBtn = document.querySelector('#prev');
+
+nextBtn.addEventListener('click', ()=>{
+    if(currentitem>=reviews.length)
+        currentitem=1
+    else
+        currentitem+=1
+    reviewPerson(currentitem)
+})
+
+prevBtn.addEventListener('click', ()=>{
+    if(currentitem<=1)
+        currentitem=reviews.length
+    else
+        currentitem-=1
+    reviewPerson(currentitem)
+})
+
+setInterval(() => {
+    nextBtn.click()
+}, 5000);
+
+//   review section ends
